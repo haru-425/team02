82b6954b4674f244960108a376873d32d40a66ba
kyouhei2525 <hozonfuniguru1@gmail.com>
2024-11-12 13:32:50 +0900

    add

--- project/SourceCode/bomb.cpp
+++ project/SourceCode/bomb.cpp
@@ -8,15 +8,19 @@ Bomb bomb;
 extern PLAYER player;
 POINT point;
 POINT screenPoint;
-
+float force = 0.0f;
+float angle = 0.0f;
+float flepX = 0.0f;
+float flepY = 0.0f;
 void bomb_init()
 {
 	bomb.bomb_state = 0;
 	bomb.bomb_position.x = -100;
 	bomb.bomb_position.y = -100;
 	bomb.bomb_speed = 0.0f;
-	bomb.bomb_angle = 0;
+	bomb.bomb_angle = 0.0f;
 	bomb.bom_time=0;
+	bomb.bomb_blast = 0.0f;
 }
 
 void bomb_deinit()
@@ -34,45 +38,58 @@ void bomb_throw()
 
 	bomb.start_bomb_position = player.position;
 
+	 //マウスカーソルの現在位置を取得
+	GetCursorPos(&point);
 
-	// マウスカーソルの現在位置を取得
-	//GetCursorPos(&point);
+	// スクリーン座標をクライアント座標に変換
+	ScreenToClient(window::getHwnd(), &point);
 
-	//// スクリーン座標をクライアント座標に変換
-	//ScreenToClient(window::getHwnd(), &point);
+	VECTOR2 Point = { (float)point.x, (float)point.y };
+	bomb.bomb_angle = tracking(Point, player.position);
 
-	//VECTOR2 Point = { point.x, point.y };
-	//bomb.bomb_angle = tracking(Point, player.position);
+
+	bomb.bomb_speed = 50.0f;
 
 	bomb.bomb_state++;
 }
 
 void bomb_update()
 {
-	if (bomb.bomb_state!=1)
+	debug::setString("%f", bomb.start_bomb_position.x);
+	switch (bomb.bomb_state)
 	{
+	case 3:
+		flepX = player.position.x - bomb.bomb_position.x;
+		flepY = player.position.y - bomb.bomb_position.y;
+		force = sqrtf(flepX * flepX + flepY * flepY) - BOMB_BLAST_STRANGE;
+		angle = tracking(player.position, bomb.bomb_position);
+		bomb_deinit();
+		break;
+
+	case 1:
+		bomb.bom_time = 0.0f;
+		bomb.bomb_state++;
+
+	case 2:
+
+		bomb.bom_time += 0.1f;
+		bomb.bomb_position = bomb.start_bomb_position + LaunchCalculatePosition(ToDegree(-bomb.bomb_angle), bomb.bomb_speed, bomb.bom_time);
+
+		if (bomb.bomb_position.y >= SCREEN_H)
+		{
+			bomb_deinit();
+		}
+	default:
+
+		player_movement(angle, force);
 		return;
-	}
-	bomb.bomb_position = bomb.start_bomb_position + LaunchCalculatePosition(bomb.bomb_angle, bomb.bomb_speed, bomb.bom_time);
-	/*if (true)
-	{
-		bomb_explosion();
-	}*/
-	if (bomb.bomb_position.y == SCREEN_H)
-	{
-		void bomb_deinit();
+
+		break;
 	}
 
-}
-
-void bomb_explosion()
-{
-	float flepX = player.position.x - bomb.bomb_position.x;
-	float flepY = player.position.y - bomb.bomb_position.y;
-	
 }
 
 void bomb_render()
 {
-	primitive::circle(bomb.bomb_position.x, bomb.bomb_position.y, 1, 1, 1);
+	primitive::circle(bomb.bomb_position.x, bomb.bomb_position.y, 10, 1, 1);
 }
--- project/SourceCode/bomb.h
+++ project/SourceCode/bomb.h
@@ -4,24 +4,27 @@
 
 #include "../GameLib/game_lib.h"
 #include <DirectXMath.h>
+
 using namespace DirectX;
+
+#define BOMB_BLAST_STRANGE 5.0f
 
 void bomb_init();			// 爆弾の初期化
 void bomb_deinit();			// 爆弾のリセット
 void bomb_throw();			// プレイヤーが爆弾を投げる
 void bomb_update();			// 爆弾の状態（タイマーなど）を更新
-void bomb_explosion();		// 爆弾の爆発処理
 void bomb_render();			// 爆弾と爆風範囲の描画処理
 
 class Bomb
 {
 public:
 	int bomb_state;
-	int bom_time;
 	VECTOR2 bomb_position;
 	VECTOR2 start_bomb_position;
+	float bom_time;
 	float bomb_speed;
 	float bomb_angle;
+	float bomb_blast;
 };
 
 #endif // BOMB_H
