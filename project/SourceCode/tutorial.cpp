f491a972cd3ac4639542c3a317cb06893b66fcb1
kyouhei2525 <hozonfuniguru1@gmail.com>
2024-11-22 11:12:42 +0900

    add

---
+++ project/SourceCode/tutorial.cpp
@@ -0,0 +1,76 @@
+//------< インクルード >---------------------------------------------------------
+#include "../GameLib/game_lib.h"
+#include "common.h"
+#include "audio.h"
+#include "m_scene.h"
+// namespace
+using namespace GameLib;
+using namespace input;
+
+//------< 定数 >----------------------------------------------------------------
+
+
+//------< 変数 >----------------------------------------------------------------
+S_SCENE start_state;
+int start_timer;
+VECTOR3 start_color;
+int tutorial_state;
+
+void start_init()
+{
+	start_state.state = 0;
+	start_timer = 0;
+	tutorial_state = 0;
+}
+
+void start_deinit()
+{
+
+	//music::stop(0);
+
+}
+
+void start_update()
+{
+	switch (start_state.state)
+	{
+	case start_state.INITIALIZE:
+		start_state.state = start_state.B_TRANSIATON;
+	case start_state.B_TRANSIATON:
+		if (true)
+		{
+			start_state.state = start_state.PARAMETER;
+		}
+		break;
+
+	case start_state.PARAMETER:
+		GameLib::setBlendMode(Blender::BS_ALPHA);
+
+		start_state.state = start_state.NORMAL;
+
+	case start_state.NORMAL:
+		/*switch (switch_on)
+		{
+		default:
+			break;
+		}*/
+
+
+
+		break;
+	case start_state.F_TRANSITION:
+		if (start_timer >= 240)
+		{
+			nextScene = SCENE_TYPE::GAME;
+		}
+
+	}
+
+	start_timer++;
+}
+
+void start_render()
+{
+	// 画面を青で塗りつぶす
+	GameLib::clear(0.3f, 0.5f, 1.0f);
+}
